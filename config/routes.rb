2fb99a128e5abff5cedee1ee38bc20e117c2d517
aecordova <ae_cordova@outlook.com>
2020-05-30 13:46:42 -0500

    Add namespaced api routes, controller folder and files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :activities, only: [:index]
+    resources :babies, only: [:index, :show]
+    resources :activity_logs, only: [:index, :update]
+  end
 end
