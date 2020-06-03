4f6429a0755b0b56e298a2124ce9d157fdece4ac
aecordova <ae_cordova@outlook.com>
2020-06-03 18:26:27 -0500

    Add eager load on Activity logs controller

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,7 +1,5 @@
 class ActivityLogsController < ApplicationController
   def index
-    @activity_logs = ActivityLog.all
+    @activity_logs = ActivityLog.all.includes(:baby, :assistant, :activity).newest_first
   end
-
-
 end
