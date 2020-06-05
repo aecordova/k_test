15d267fff5ce960c3fad2bad01ad5f9ede6d7fd2
aecordova <ae_cordova@outlook.com>
2020-06-05 03:49:49 -0500

    correct filtering not working

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,17 +1,17 @@
 document.addEventListener('turbolinks:load', () => {
-  if (window.location.pathname === '/activity_logs') {
-    const filterSubmit = document.getElementById('filter-submit-btn');
-    const babyFilter = document.getElementById('baby-filter');
-    const assistantFilter = document.getElementById('assistant-filter');
-    const statusFilter = document.getElementById('status-filter');
-    const dataTable = document.getElementById('acts-table-body');
+  const filterSubmit = document.getElementById('filter-submit-btn');
+  const babyFilter = document.getElementById('baby-filter');
+  const assistantFilter = document.getElementById('assistant-filter');
+  const statusFilter = document.getElementById('status-filter');
+  const dataTable = document.getElementById('acts-table-body');
 
-    console.log(window.location);
+  console.log(window.location);
 
-    const babyColumn = 0;
-    const assistantColumn = 1;
-    const statusColumn = 4;
+  const babyColumn = 0;
+  const assistantColumn = 1;
+  const statusColumn = 4;
 
+  if (filterSubmit) {
     filterSubmit.addEventListener('click', () => {
       const filters = [
         babyFilter.value,
@@ -21,37 +21,36 @@ document.addEventListener('turbolinks:load', () => {
       const columns = [babyColumn, assistantColumn, statusColumn];
       filterTable(dataTable, filters, columns, 'Todos');
     });
-
-    const filterTable = (table, filters, columns, all) => {
-      clearFilter(table);
-      const { rows } = table;
-      const matches = [];
-      for (let i = 0; i < rows.length; i += 1) {
-        const cellValues = [];
-        const matched = false;
-        columns.forEach((c) => {
-          cellValues.push(rows[i].cells[c].textContent.toUpperCase());
-        });
-        cellValues.forEach((v, ind) => {
-          if (filters[ind] === '' || filters[ind] === all) {
-            matches[ind] = true;
-          } else if (v === filters[ind].toUpperCase()) {
-            matches[ind] = true;
-          } else {
-            matches[ind] = false;
-          }
-        });
-
-        if (matches.includes(false)) {
-          rows[i].style.display = 'none';
-        }
-      }
-    };
-
-    const clearFilter = (table) => {
-      for (let i = 0; i < table.rows.length; i += 1) {
-        table.rows[i].style.display = '';
-      }
-    };
   }
+  const filterTable = (table, filters, columns, all) => {
+    clearFilter(table);
+    const { rows } = table;
+    const matches = [];
+    for (let i = 0; i < rows.length; i += 1) {
+      const cellValues = [];
+      const matched = false;
+      columns.forEach((c) => {
+        cellValues.push(rows[i].cells[c].textContent.toUpperCase());
+      });
+      cellValues.forEach((v, ind) => {
+        if (filters[ind] === '' || filters[ind] === all) {
+          matches[ind] = true;
+        } else if (v === filters[ind].toUpperCase()) {
+          matches[ind] = true;
+        } else {
+          matches[ind] = false;
+        }
+      });
+
+      if (matches.includes(false)) {
+        rows[i].style.display = 'none';
+      }
+    }
+  };
+
+  const clearFilter = (table) => {
+    for (let i = 0; i < table.rows.length; i += 1) {
+      table.rows[i].style.display = '';
+    }
+  };
 });
